5770933f87ee981b801ee20da038a813706c95d1
adian juan <juanrhamadhan@gmail.com>
2025-03-27 13:11:42 +0700

    update comment

--- services/send-wa.go
+++ services/send-wa.go
@@ -41,19 +41,6 @@ func sendWaMessage(id int, to, msg, imgUrl string, scheduledAt time.Time) {
 		},
 	}
 
-	// Send the POST request with headers
-	// resp, err := clientResty.R().
-	// 	SetHeaders(map[string]string{
-	// 		// "Authorization": "defaultDS-49434e96f251d2ff",
-	// 		// "x-api-key":     "23b964f4c543ccdc",
-	// 		// "jwt":           util.JWT,
-	// 		"Accept":       "application/json",
-	// 		"Content-Type": "application/json",
-	// 		"token":        configuration.CONFIG.DaisiApiToken,
-	// 	}).
-	// 	SetBody(payload).
-	// 	Post(configuration.CONFIG.DaisiApiUrl)
-
 	// if err != nil {
 	// 	log.Fatalf("Error occurred while sending message: %v", err)
 	// }
